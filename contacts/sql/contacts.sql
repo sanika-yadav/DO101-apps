4f27ac137667b96306b4671b7fd73e54b60bc764
sanika-yadav <sanikayadav2006@gmail.com>
2025-01-30 21:48:28 +0530

    Update contacts.sql

--- contacts/sql/contacts.sql
+++ contacts/sql/contacts.sql
@@ -1,5 +1,5 @@
 drop table if exists contacts;
-
+/*create table*/
 create table contacts(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null);
 
 insert into contacts(firstname, lastname, email) values
